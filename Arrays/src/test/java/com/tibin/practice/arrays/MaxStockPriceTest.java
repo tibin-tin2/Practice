3764d25afc45af20c0fc078d0bcc5aedff82af43
TIBIN <tibintomy333@gmail.com>
2024-09-15 18:41:46 +0530

    added MaxStockPrice problem

--- Arrays/src/test/java/com/tibin/practice/arrays/MaxStockPriceTest.java
+++ Arrays/src/test/java/com/tibin/practice/arrays/MaxStockPriceTest.java
@@ -8,29 +8,29 @@ class MaxStockPriceTest {
 
     @Test
     public void testMaxProfit() {
-        int prices[] = {6, 1, 5, 6, 1, 8, 7};
-        int exprectMaxProfit = 7;
+        int[] prices = {6, 1, 5, 6, 1, 8, 7};
+        int expectMaxProfit = 7;
 
         MaxStockPrice maxStockPrice = new MaxStockPrice();
-        assertEquals(exprectMaxProfit, maxStockPrice.maxProfit(prices));
+        assertEquals(expectMaxProfit, maxStockPrice.maxProfit(prices));
     }
 
     @Test
     public void testMaxProfit1() {
-        int prices[] = {1, 1, 12, 1, 1, 13, 7};
-        int exprectMaxProfit = 12;
+        int[] prices = {1, 1, 12, 1, 1, 13, 7};
+        int expectMaxProfit = 12;
 
         MaxStockPrice maxStockPrice = new MaxStockPrice();
-        assertEquals(exprectMaxProfit, maxStockPrice.maxProfit(prices));
+        assertEquals(expectMaxProfit, maxStockPrice.maxProfit(prices));
     }
 
     @Test
     public void testMaxProfitWithNoProfit() {
-        int prices[] = {12, 11, 10, 9, 8 , 7, 6, 5, 4};
-        int exprectMaxProfit = 0;
+        int[] prices = {12, 11, 10, 9, 8 , 7, 6, 5, 4};
+        int expectMaxProfit = 0;
 
         MaxStockPrice maxStockPrice = new MaxStockPrice();
-        assertEquals(exprectMaxProfit, maxStockPrice.maxProfit(prices));
+        assertEquals(expectMaxProfit, maxStockPrice.maxProfit(prices));
     }
 
 }
