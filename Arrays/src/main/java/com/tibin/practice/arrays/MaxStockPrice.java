d9a47bae6b978c05102633e52272020420f03ab9
TIBIN <tibintomy333@gmail.com>
2024-09-15 18:42:16 +0530

    added MaxStockPrice problem

--- Arrays/src/main/java/com/tibin/practice/arrays/MaxStockPrice.java
+++ Arrays/src/main/java/com/tibin/practice/arrays/MaxStockPrice.java
@@ -4,9 +4,7 @@ package com.tibin.practice.arrays;
  * <b>Ques:</b> You are given an array of prices where prices[i] is the price of a given stock on an ith day.
  * You want to maximise your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
  * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
- *
- * <br><br>
- *
+ * <p>
  * See {@link com.tibin.practice.arrays.MaxStockPriceTest MaxStockPriceTest} for the test cases.
  */
 public class MaxStockPrice {
